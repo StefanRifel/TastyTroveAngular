8bde5b154ad4a8068be496ec3aaf82be5eb66f73
Stefan Rifel <s.rifel23i@gmail.com>
2024-03-10 09:56:02 +0100

    feat: add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//ingredient form imports
+import {Ingredient} from "./core/models/ingredient";
+import {IngredientFilterComponent} from "./features/ingredient/component/ingredient-filter/ingredient-filter.component";
+import {IngredientFormComponent} from "./features/ingredient/component/ingredient-form/ingredient-form.component";
+import {IngredientListComponent} from "./features/ingredient/component/ingredient-list/ingredient-list.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IngredientListComponent, IngredientFormComponent, IngredientFilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
